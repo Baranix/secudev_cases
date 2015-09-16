055005a94f668f9249094da0dd06ad2dc5bf289a
Nikki <oathofrose@gmail.com>
2015-09-16 20:10:20 +0800

    Updated database, removed unnecessary whitespace, added HTML of the textarea for message posting.

--- secudev_case_2.sql
+++ secudev_case_2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2015 at 06:26 AM
+-- Generation Time: Sep 16, 2015 at 02:08 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -19,6 +19,66 @@ SET time_zone = "+00:00";
 --
 -- Database: `secudev_case_2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+
+CREATE TABLE IF NOT EXISTS `gender` (
+  `id` tinyint(1) NOT NULL,
+  `gender` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gender`
+--
+
+INSERT INTO `gender` (`id`, `gender`) VALUES
+(1, 'M'),
+(2, 'F');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `edited_on` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salutation`
+--
+
+CREATE TABLE IF NOT EXISTS `salutation` (
+  `id` int(11) NOT NULL,
+  `salutation` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `salutation`
+--
+
+INSERT INTO `salutation` (`id`, `salutation`) VALUES
+(1, 'Mr.'),
+(2, 'Sir'),
+(3, 'Señor'),
+(4, 'Count'),
+(5, 'Miss'),
+(6, 'Ms.'),
+(7, 'Mrs.'),
+(8, 'Madame'),
+(9, 'Majesty'),
+(10, 'Señora');
 
 -- --------------------------------------------------------
 
@@ -52,6 +112,24 @@ INSERT INTO `user` (`id`, `username`, `password`, `salutation`, `first_name`, `l
 --
 
 --
+-- Indexes for table `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `salutation`
+--
+ALTER TABLE `salutation`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -61,6 +139,11 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
